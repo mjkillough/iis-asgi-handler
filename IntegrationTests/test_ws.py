6271ada42f91851fa60f245f83aafaf02e02c464
Michael Killough <michaeljkillough@gmail.com>
2016-05-19 21:56:26 +0000

    A complete mess that vaguely does WebSockets reads.
    Committing before reworking the crap outta it.

--- IntegrationTests/test_ws.py
+++ IntegrationTests/test_ws.py
@@ -15,6 +15,10 @@ from websocket import create_connection
 
 def test_ws_basic(site, asgi):
     ws = create_connection(site.ws_url, timeout=5)
+    ws.send("Hello, world");
+    channel, data = asgi.channels.receive_many(['websocket.receive'], block=True)
+    print(channel, data)
+    assert False
 
 if __name__ == '__main__':
     # Should really sys.exit() this, but it causes Visual Studio
--- RedisAsgiHandlerLib/WsRequestHandler.cpp
+++ RedisAsgiHandlerLib/WsRequestHandler.cpp
@@ -1,5 +1,6 @@
 #define WIN32_LEAN_AND_MEAN
 #include <httpserv.h>
+#include <iiswebsocket.h>
 #include <ppltasks.h>
 
 #include "WsRequestHandler.h"
@@ -24,6 +25,8 @@ REQUEST_NOTIFICATION_STATUS WsRequestHandler::OnAsyncCompletion(IHttpCompletionI
     HRESULT hr = completion_info->GetCompletionStatus();
     DWORD bytes = completion_info->GetCompletionBytes();
 
+    StartReadWritePipelines();
+
     logger.debug() << typeid(*m_current_connect_step.get()).name() << "->OnAsyncCompletion() being called";
     auto result = m_current_connect_step->OnAsyncCompletion(hr, bytes);
     logger.debug() << typeid(*m_current_connect_step.get()).name() << "->OnAsyncCompletion() = " << result;
@@ -31,14 +34,34 @@ REQUEST_NOTIFICATION_STATUS WsRequestHandler::OnAsyncCompletion(IHttpCompletionI
     return HandlerStateMachine(m_current_connect_step, result);
 }
 
+void WsRequestHandler::StartReadWritePipelines()
+{
+    m_read_pump.Start();
+}
+
+
+// Connection Pipeline - AcceptWebSocketStep
 
 StepResult AcceptWebSocketStep::Enter()
 {
+    // Setting the status as 101.
     auto resp = m_http_context->GetResponse();
-    // Setting the status as 101 and Flushing causes the IIS WebSocket module to
-    // kick into action. We have to set fMoreData=true in order for it to work.
     resp->SetStatus(101, "");
-    resp->Flush(true, true, nullptr, nullptr);
+
+    // ... and Flushing causes the IIS WebSocket module to kick into action.
+    // We have to set fMoreData=true in order for it to work.
+    DWORD num_bytes = 0;
+    BOOL completion_expected = false;
+    HRESULT hr = resp->Flush(true, true, &num_bytes, &completion_expected);
+    if (FAILED(hr)) {
+        logger.debug() << "Flush() = " << hr;
+        return kStepFinishRequest;
+    }
+
+    if (!completion_expected) {
+        return OnAsyncCompletion(S_OK, num_bytes);
+    }
+
     return kStepAsyncPending;
 }
 
@@ -55,6 +78,8 @@ std::unique_ptr<RequestHandlerStep> AcceptWebSocketStep::GetNextStep()
     return std::make_unique<SendConnectToApplicationStep>(m_handler);
 }
 
+
+// Connection Pipeline - SendConnectToApplicationStep
 
 StepResult SendConnectToApplicationStep::Enter()
 {
@@ -78,13 +103,95 @@ StepResult SendConnectToApplicationStep::OnAsyncCompletion(HRESULT hr, DWORD num
         return kStepFinishRequest;
     }
 
-    // We return kStepFinishRequest to signal the end of this connection pipeline,
-    // but it won't actually complete the request. (We need to send
-    // IWebSocketContext->SendConnectionClose() for that).
-    return kStepFinishRequest;
+    // We return kStepAsyncPending so that the request is kept open. We'll
+    // get callbacks from IWebSocketContext from here onwards (not directly from IIS'
+    // main request pipeline).
+    return kStepAsyncPending;
 }
 
 std::unique_ptr<RequestHandlerStep> SendConnectToApplicationStep::GetNextStep()
 {
+    // TODO: Go to DummyStep in order to make sure we release any resources held
+    //       by this step?
     throw std::runtime_error("SendConnectToApplicationStep::GetNextStep() should never get called.");
+}
+
+
+WsIoPump::WsIoPump(WsRequestHandler& handler)
+    : logger(handler.logger), m_channels(handler.m_channels),
+      m_http_context(handler.m_http_context)
+{ }
+
+
+void WsReadPump::Start()
+{
+    auto http_context3 = static_cast<IHttpContext3*>(m_http_context);
+    m_ws_context = static_cast<IWebSocketContext*>(
+        http_context3->GetNamedContextContainer()->GetNamedContext(L"websockets")
+    );
+
+    ReadAsync();
+}
+
+void WsReadPump::ReadAsync()
+{
+    logger.debug() << "ReadAsync()";
+
+    BOOL completion_expected = false;
+    while (!completion_expected) {
+        DWORD num_bytes = m_msg.data.size();
+        BOOL utf8 = false, final_fragment = false, close = false;
+
+        HRESULT hr = m_ws_context->ReadFragment(
+            m_msg.data.data(), &num_bytes, true, &utf8, &final_fragment, &close,
+            ReadCallback, this, &completion_expected
+        );
+        if (FAILED(hr)) {
+            logger.debug() << "ReadFragment() = " << hr;
+            // TODO: Call an Error() or something.
+            // TODO: Figure out how to close the request from here.
+        }
+
+        if (!completion_expected) {
+            logger.debug() << "ReadFragment() returned completion_expected=false";
+            ReadAsyncComplete(S_OK, num_bytes, utf8, final_fragment, close);
+        }
+    }
+
+    logger.debug() << "ReadAsync() returning";
+}
+
+void WsReadPump::ReadAsyncComplete(HRESULT hr, DWORD num_bytes, BOOL utf8, BOOL final_fragment, BOOL close)
+{
+    logger.debug() << "ReadAsyncComplete()";
+
+    if (FAILED(hr)) {
+        logger.debug() << "ReadAsyncComplete() hr = " << hr;
+        // TODO: Figure out how to propogate an error from here.
+    }
+
+    logger.debug() << "ReadAsyncComplete() " << num_bytes << "  " << utf8 << " " << final_fragment << " " << close;
+    m_msg.data_size += num_bytes;
+    // TODO: Resize the buffer if the message is not complete and we're almost
+    // at the end of the buffer.
+    SendToApplicationAsync();
+}
+
+void WsReadPump::SendToApplicationAsync()
+{
+    // Send synchronously for now.
+    msgpack::sbuffer buffer;
+    msgpack::pack(buffer, m_msg);
+    m_channels.Send("websocket.receive", buffer);
+
+    // Reset for the next msg.
+    m_msg.data.resize(AsgiWsReceiveMsg::BUFFER_CHUNK_SIZE);
+    m_msg.order += 1;
+}
+
+void WINAPI WsReadPump::ReadCallback(HRESULT hr, VOID* context, DWORD num_bytes, BOOL utf8, BOOL final_fragment, BOOL close)
+{
+    auto read_pump = static_cast<WsReadPump*>(context);
+    read_pump->ReadAsyncComplete(hr, num_bytes, utf8, final_fragment, close);
+    read_pump->ReadAsync();
 }
