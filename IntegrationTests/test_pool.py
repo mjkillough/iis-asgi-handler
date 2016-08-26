b6809fe3a4d7180eddf1eddba28fd956cdd21435
Michael Killough <michaeljkillough@gmail.com>
2016-08-26 14:15:26 +0000

    Add tests for stopping/changing pools.
    For some reason the 'stop pool and check the processes terminate' is
    failing. Mark as a known failure - probably best to investigate once we
    have logging for ProcessPool.dll

--- IntegrationTests/test_pool.py
+++ IntegrationTests/test_pool.py
@@ -23,16 +23,75 @@ def get_processes_for_user(user):
     }
 
 
-def test_pool_launches_process(site, session):
-    site.add_process_pool('C:\\Python27\\pythonw.exe', '-c "while True: pass"')
+_pythonw = 'C:\\Python27\\pythonw.exe'
 
+
+def test_pool_launches_process(site, session):
+    site.add_process_pool(_pythonw, '-c "while True: pass"')
     before = get_processes_for_user(site.user)
 
-    # Then when we make the first request, we should see the process
-    # get started.
+    # Make a request so the pool gets started.
     session.get(site.url + site.static_path).result(timeout=2)
 
     after = get_processes_for_user(site.user)
     assert after == before | {
-        ('pythonw.exe', ('C:\\Python27\\pythonw.exe', '-c', 'while True: pass')),
+        ('pythonw.exe', (_pythonw, '-c', 'while True: pass')),
+    }
+
+
+@pytest.mark.xfail
+def test_pool_terminated_when_site_stopped(site, session):
+    site.add_process_pool(_pythonw, '-c "while True: pass"')
+    before = get_processes_for_user(site.user)
+
+    # Make a request so the pool gets started.
+    session.get(site.url + site.static_path).result(timeout=2)
+
+    after1 = get_processes_for_user(site.user)
+    assert after1 == before | {
+        ('pythonw.exe', (_pythonw, '-c', 'while True: pass')),
+    }
+
+    site.stop()
+    after2 = get_processes_for_user(site.user)
+    assert before == after2
+
+
+
+def test_pool_two_pools(site, session):
+    site.add_process_pool(_pythonw, '-c "while True: a=1"')
+    site.add_process_pool(_pythonw, '-c "while True: a=2"')
+    before = get_processes_for_user(site.user)
+
+    # Make a request so the pool gets started.
+    session.get(site.url + site.static_path).result(timeout=2)
+
+    after = get_processes_for_user(site.user)
+    assert after == before | {
+        ('pythonw.exe', (_pythonw, '-c', 'while True: a=1')),
+        ('pythonw.exe', (_pythonw, '-c', 'while True: a=2')),
+    }
+
+
+def test_pool_add_to_existing_site(site, session):
+    site.add_process_pool(_pythonw, '-c "while True: a=1"')
+    before = get_processes_for_user(site.user)
+
+    # Make a request so the pool gets started.
+    session.get(site.url + site.static_path).result(timeout=2)
+
+    after1 = get_processes_for_user(site.user)
+    assert after1 == before | {
+        ('pythonw.exe', (_pythonw, '-c', 'while True: a=1')),
+    }
+
+    site.add_process_pool(_pythonw, '-c "while True: a=2"')
+
+    # Make another request so the pool gets started again.
+    session.get(site.url + site.static_path).result(timeout=2)
+
+    after2 = get_processes_for_user(site.user)
+    assert after2 == before | {
+        ('pythonw.exe', (_pythonw, '-c', 'while True: a=1')),
+        ('pythonw.exe', (_pythonw, '-c', 'while True: a=2')),
     }
