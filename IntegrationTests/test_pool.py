09902ddee5ab31b7776cd9bd3905b3f2f89dbcfc
Michael Killough <michaeljkillough@gmail.com>
2016-08-26 11:38:21 +0000

    Allow other processes to be running before the test.
    And assert that our new process is running afterwards. Things like
    WerFault and VS JIT debugger might be running as the pool user.

--- IntegrationTests/test_pool.py
+++ IntegrationTests/test_pool.py
@@ -17,23 +17,21 @@ def get_processes_for_user(user):
                 procs.append(proc)
         except psutil.AccessDenied:
             pass
-    return procs
+    return {
+        (proc.name(), tuple(proc.cmdline()))
+        for proc in procs
+    }
 
 
 def test_pool_launches_process(site, session):
     user = 'IIS APPPOOL\\asgi-test-pool'
-    # Initially, there shouldn't be any processes running except the
-    # IIS application pool.
-    assert get_processes_for_user(user) == []
+    before = get_processes_for_user(user)
 
     # Then when we make the first request, we should see the process
     # get started.
     session.get(site.url + site.static_path).result(timeout=2)
 
-    processes = {
-        (proc.name(), tuple(proc.cmdline()))
-        for proc in get_processes_for_user(user)
-    }
-    assert processes == {
+    after = get_processes_for_user(user)
+    assert after == before | {
         ('pythonw.exe', ('C:\\Python27\\pythonw.exe', '-c', 'while True: pass')),
     }
